963d799ff8c63a4785865074a37f12e7f8cb5257
Angharad Beazley <a.beazley@elsevier.com>
2020-09-20 21:40:19 +0100

    first commit

---
+++ src/Pages/PersonInfo/PersonInfo.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
+//back button need its event to call props.backToMainPage(true) 
+// to go back to main page
+
+function PersonInfo(props) {
+    const  handleBackButtonClick= (event) => {
+        props.backToMainPage(true);
+        
+    }
+       return (
+           <div>
+               //personal information output with html
+            Name: {props.personData}
+            
+            <Button
+                variant="primary"
+                onClick={handleBackButtonClick}
+            ></Button>
+           </div>
+
+       ) 
+   }
+       export default PersonInfo;
+
