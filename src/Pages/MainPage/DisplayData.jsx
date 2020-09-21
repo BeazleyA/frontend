af179e39f42ac467f788b04afffdcfc532eb3d2c
Angharad Beazley <a.beazley@elsevier.com>
2020-09-21 14:19:23 +0100

    added .json file

--- src/Pages/MainPage/DisplayData.jsx
+++ src/Pages/MainPage/DisplayData.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 
-function DisplayData(props) {
+// function DisplayData(props) {
 
- //   props.data = the data about the people;
-//create function to make data readable
-//loop for going through the data (called in main)
-    return (
-        <div>
-            //where the html to display data will be
-        </div>
-    ) 
-}
-    export default DisplayData;
+//  //   props.data = the data about the people;
+// //create function to make data readable
+// //loop for going through the data (called in main)
+//     return (
+//         <div>
+//             //where the html to display data will be
+//         </div>
+//     ) 
+// }
+//     export default DisplayData;
--- src/Pages/PersonInfo/PersonInfo.js
+++ src/Pages/PersonInfo/PersonInfo.js
@@ -1,25 +1,27 @@
 import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
+
+
 //back button need its event to call props.backToMainPage(true) 
 // to go back to main page
 
-function PersonInfo(props) {
-    const  handleBackButtonClick= (event) => {
-        props.backToMainPage(true);
+// function PersonInfo(props) {
+//     const  handleBackButtonClick= (event) => {
+//         props.backToMainPage(true);
         
-    }
-       return (
-           <div>
-               //personal information output with html
-            Name: {props.personData}
+//     }
+//        return (
+//            <div>
+//                //personal information output with html
+//             Name: {props.personData}
             
-            <Button
-                variant="primary"
-                onClick={handleBackButtonClick}
-            ></Button>
-           </div>
+//             <Button
+//                 variant="primary"
+//                 onClick={handleBackButtonClick}
+//             ></Button>
+//            </div>
 
-       ) 
-   }
-       export default PersonInfo;
+//        ) 
+//    }
+//        export default PersonInfo;
 
